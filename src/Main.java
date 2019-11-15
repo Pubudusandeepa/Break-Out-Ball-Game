20ca1ae4ff356b3fba02efa6f5f20dcd9c146232
Pubudusandeepa <39061732+Pubudusandeepa@users.noreply.github.com>
2019-11-15 18:33:34 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pubudu Sandeepa
+ */
+public class Main {
+    public static void main(String[] args){
+        JFrame obj = new JFrame();
+        Gameplay gamePlay = new Gameplay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+        
+    }
+    
+}
